6450ae33cc73e7239b8a39353e6e33e1a16769d2
IrinaPeresunko <peresunkoi@gmail.com>
2015-05-07 22:46:47 +0300

    add method that finds the reference element of the array

--- QuickSort/src/quickSortWithRecursion/QuickSortWithRecursion.java
+++ QuickSort/src/quickSortWithRecursion/QuickSortWithRecursion.java
@@ -5,10 +5,79 @@ import java.util.Arrays;
 public class QuickSortWithRecursion {
 	private int[] ODarray;
 	private int[][] TDarray;
-	private int first;
-	private int last;
+//	private int first;
+//	private int last;
 	private int middle;
 	
+	public int getMiddleElement(){
+		return middle;
+	}
+	private int getMin(){
+		int min;
+		if(ODarray!=null){
+			min=ODarray[0];
+			for(int i=0;i<ODarray.length;i++){
+				if(min>ODarray[i]){
+					min = ODarray[i];
+				}
+			}
+		}
+		else{
+			min = TDarray[0][0];
+			for(int i=0;i<TDarray.length;i++){
+				for(int j=0;j<TDarray[i].length;j++){
+					if(min>TDarray[i][j]){
+						min = TDarray[i][j];
+					}
+				}
+			}
+		}
+		return min;
+	}
+	private int getMax(){
+		int max=0;
+		if(ODarray!=null){
+			max=ODarray[0];
+			for(int i=0;i<ODarray.length;i++){
+				if(max<ODarray[i]){
+					max = ODarray[i];
+				}
+			}
+		}
+		else{
+			max = TDarray[0][0];
+			for(int i=0;i<TDarray.length;i++){
+				for(int j=0;j<TDarray[i].length;j++){
+					if(max<TDarray[i][j]){
+						max = TDarray[i][j];
+					}
+				}
+			}
+		}
+		return max;
+	}
+	private int calculateMiddleElement(){
+		return (getMax()+getMin())/2;
+	}
+	public void print(){
+		if(ODarray!=null){
+			System.out.println(Arrays.toString(ODarray));
+		}
+		else{
+			for(int i=0;i<TDarray.length;i++){
+				for(int j=0;j<TDarray[i].length;j++){
+					System.out.print(TDarray[i][j]+" ");
+				}
+				System.out.println();
+			}		
+		}
+	}
+	public Object getArray(){
+		if(ODarray!=null){
+			return ODarray;
+		}
+		return TDarray;
+	}
 	public int[] getODArray(){
 		return ODarray;
 	}
@@ -18,7 +87,7 @@ public class QuickSortWithRecursion {
 	private int[] createOneDimensionalArray(int N){
 		ODarray = new int[N];
 		for(int i=0;i<N;i++){
-			ODarray[i] = (int) Math.random()%10;
+			ODarray[i] = (int) (Math.random()*20);
 		}
 		return ODarray;
 	}
@@ -29,37 +98,39 @@ public class QuickSortWithRecursion {
 		}
 		for(int i=0;i<N;i++){
 			for(int j=0;j<N;j++){
-				TDarray[i][j]  = (int) Math.random();
+				TDarray[i][j]  = (int) (Math.random()*20);
 			}
 		}
 		return TDarray;
 		
 	}
-	public QuickSortWithRecursion(int[] array,int first,int last){
+	public QuickSortWithRecursion(int[] array){
 		this.ODarray=array;
-		if(first>=0 && last>=0){
-			this.first=first;
-			this.last = last;
-			this.middle=(first+last)/2;
-		}
-		else{
-			System.out.println("The input numbers are not correct for this program");
-			this.first=this.last = this.middle=0;
-		}
+		this.middle=calculateMiddleElement();
+	}
+	public QuickSortWithRecursion(int[][] array){
+		this.TDarray=array;
+		this.middle=calculateMiddleElement();
 	}
 	public QuickSortWithRecursion(int dimension,int countOfElements){
 		if(dimension==1){
 			createOneDimensionalArray(countOfElements);
+			this.middle=calculateMiddleElement();
 		}
 		else{
 			createTwoDimensionalArray(countOfElements);
+			this.middle=calculateMiddleElement();
 		}
 	}
 	public static void main(String[] args){
 		QuickSortWithRecursion qswr = new QuickSortWithRecursion(1,20);
-		System.out.println(qswr.getODArray().toString());
-		System.out.println(Arrays.toString(qswr.getODArray()));
+		//System.out.println(Arrays.toString((int[]) qswr.getArray()));
 		
+		QuickSortWithRecursion qswr1 = new QuickSortWithRecursion(2,5);
+		qswr.print();
+		System.out.println("middle = "+qswr.getMiddleElement());
+		qswr1.print();
+		System.out.println("middle = "+qswr1.getMiddleElement());
 	}
 
 }
